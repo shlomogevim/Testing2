288e23d56f956074011b823e966552b95c90d2b5
Shlomo <shlomo.gevim@gmail.com>
2019-11-03 19:33:36 +0200

    Testing2

---
+++ app/src/main/java/com/example/testing2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testing2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
